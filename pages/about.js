1af51d33d38d8bdc88487a51c48307acc5f55f98
Steph Davidson <stephcdavidson@gmail.com>
2021-06-21 19:23:40 -0400

    wow

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const about = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <h3>bop</h3>
+    </div>
+  );
+};
+
+export default about;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My sick next js page</title>
+        <meta name="keywords" content="cool, dope, fresh" />
+      </Head>
+      <h1>welcome</h1>
+    </div>
+  );
+}
